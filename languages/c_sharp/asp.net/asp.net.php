4ed263be2d8fe16c3e3732dfca8e9405ec24e18c
Alexander <Aleksandr.Dedyukhin@vsegda-da.com>
2022-06-01 00:26:21 +0400

    Доделал страницу ASP.NET

--- languages/c_sharp/asp.net/asp.net.php
+++ languages/c_sharp/asp.net/asp.net.php
@@ -1,56 +1,106 @@
+<?php
+// var_dump($_POST);
+$index = file_get_contents("index.txt");
+
+$page = fopen("pages.txt", 'r');
+$i = 0;
+while (!feof($page)) //eof - end of file
+{
+    $pages[$i] = fgets($page);
+    $i++;
+}
+$size = count($pages);
+
+if (isset($_POST['plus'])) {
+    global $index;
+    pluseOne();
+    printWeb();
+}
+
+if (isset($_POST['minus'])) {
+    global $index;
+    minusOne();
+    printWeb();
+}
+
+function printWeb()
+{
+    global $index, $pages;
+    echo '<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="../../../index.html">Главная</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <span class="navbar-text">
+                    Сайт для изучения языков программирования
+                </span>
+        </div>
+    </div>
+</nav>';
+
+    echo '<div class="center">
+            <iframe width="1280" height="720" 
+                  src=' . $pages[$index] . '
+                       title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+                       encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>';
+}
+
+function minusOne()
+{
+    global $index;
+    $writer = fopen("index.txt", 'w+');
+
+    if ($index - 1 > 0) {
+        fwrite($writer, $index - 1);
+    } else {
+        fwrite($writer, 0);
+    }
+
+    $index = file_get_contents("index.txt");
+    fclose($writer);
+}
+
+function pluseOne()
+{
+    global $index, $size;
+    $writer = fopen("index.txt", 'w+');
+
+    if ($index != null && $index < $size - 1) {
+        fwrite($writer, $index + 1);
+    } else {
+        fwrite($writer, $index);
+    }
+
+    $index = file_get_contents("index.txt");
+    fclose($writer);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF8">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="..\..\..\css\style.css" rel="stylesheet">
 </head>
 
-<style>
-    .center {
-        width: 1000px;
-        /* Ширина элемента в пикселах */
-        padding: 2px;
-        /* Поля вокруг текста */
-        margin: auto;
-        /* Выравниваем по центру */
-        display: flex;
-        background-color: transparent;
-        font-stretch: normal;
-    }
-</style>
-
-<style>
-    .colorblue {
-        color: rgb(118, 205, 216);
-        /* Цвет текста */
-    }
-</style>
-
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="../../../index.html">Главная</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <span class="navbar-text">
-                        Сайт для изучения языков программирования
-                    </span>
-            </div>
-        </div>
-    </nav>
+    <div class="center">
+        <form action="asp.net.php" method="POST">
+            <input type="hidden" value="minus" name="minus">
+            <input type="submit" value="back">
+        </form>
 
-    <iframe width="1280" height="720" src="https://www.youtube.com/embed/AWWKi9BRjhY?list=PLeY40vx99GXYyV4gIJVqL0R2aDJQ60mNW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn2373KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-        crossorigin="anonymous"></script>
+        <form action="asp.net.php" method="POST">
+            <input type="hidden" value="plus" name="plus">
+            <input type="submit" value="next">
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn2373KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
